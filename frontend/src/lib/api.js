8d17dc635f5f9271e965c45cbc814e5d9211544d
Aditya-Gupta23 <9555.adityagupta@gmail.com>
2025-07-20 00:03:22 +0530

    some work done

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -4,3 +4,8 @@ export const getAuthUser=async ()=>{
     const res=await axiosInstance("/auth/me")
     return res.data
 }
+
+export const completeOnboarding=async(userData)=>{
+    const response=await axiosInstance.post('/auth/onboarding',userData)
+    return response.data;
+}
--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -1,6 +1,27 @@
-
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import useAuthUser from '../hooks/useAuthUser.jsx'
+import completeOnboarding from '../lib/api.js'
 
 const OnboardingPage = () => {
+
+  const {authUser}=useAuthUser();
+  const queryClient = useQueryClient();
+  const [formState, setFormState] = useState({
+    fullName: authUser?.fullName || "",
+    bio: authUser?.bio || "",
+    nativeLanguage: authUser?.nativeLanguage || "",
+    learningLanguage: authUser?.learningLanguage || "",
+    location: authUser?.location || "",
+    profilePic: authUser?.profilePic || "",
+  });
+  const {mutate:onboardingMutation,isPending}=useMutation({
+    mutationFn:completeOnboardingd,
+    onSuccess=()=>{
+      toast.success("Profile onboarded successfully");
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    }
+  })
+
   return (
     <div>
       
